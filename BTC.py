7c64b5d4dfd8e4a20be34cee20e2067e446cb28e
tpbw4321 <tpbw4321@gmail.com>
2014-06-13 00:51:04 -0700

    Update BTC.py

--- BTC.py
+++ BTC.py
@@ -1,35 +1,30 @@
-
 import json
-import urllib2
+import requests
 import os
 
-btceBtc = 'https://btc-e.com/api/2/btc_usd/ticker'
-btceLtc = 'https://btc-e.com/api/2/ltc_usd/ticker'
+btceBtcAPI = 'https://btc-e.com/api/2/btc_usd/ticker'
+btceLtcAPI = 'https://btc-e.com/api/2/ltc_usd/ticker'
+btceXpmAPI = 'https://btc-e.com/api/2/xpm_btc/ticker'
 goxBTC = 'http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'
 
-def getApiData(url) :
-    return json.dumps(json.load(urllib2.urlopen(url)))
+def btceAPI(API):
+	btceTick = requests.get(API)
+	return btceTick.json()['ticker']['last']
 
-def parseGox(goxData) :
-    start = goxData.find("\"",goxData.find(" ",goxData.find("value\":",goxData.find("\"last\":"))))
-    fin = goxData.find("\"", start+1)
-    return goxData[start+1:fin]
-
-def parseBtce(btceData) :
-    start = btceData.find(" " , btceData.find("last"))
-    fin = btceData.find(",", start)
-    return btceData[start:fin]
 
 while True :
-    btceBtcData = getApiData(btceBtc)
-    btceLtcData = getApiData(btceLtc)
-    goxBtcData = getApiData(goxBTC)
+	btceBtc = float(btceAPI(btceBtcAPI))
+	btceLtc = float(btceAPI(btceLtcAPI))
+	btceXpm = float(btceAPI(btceXpmAPI))
 
-    os.system('clear')
-    
-    print "BTC/USD"
-    print "BTC-e: " + parseBtce(btceBtcData)
-    print "Mt.Gox: " + parseGox(goxBtcData)
-    print "\nLTC/USD"
-    print "BTC-e: " + parseBtce(btceLtcData)
+	os.system('clear')
+	print "BTC/USD"
+	print "BTC-e: ", btceBtc
+	print
+	print "LTC/USD"
+	print "BTC-e: ", btceLtc
+	print
+	print "XPM/BTC"
+	print "BTC-e: ", btceXpm
+
 
